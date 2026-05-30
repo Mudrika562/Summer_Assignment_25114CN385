670b7bbcf6dd9a84a9de8f9e1827745aa584defb
Mudrika Mishra <mudrika.mishra07@gmail.com>
2026-05-30 20:18:55 +0530

    DAY 3 coding questions

---
+++ DAY 3/code2.cpp
@@ -0,0 +1,30 @@
+//prime numbers in a range
+#include <stdio.h>
+
+int main() {
+    int start, end, i, j, prime;
+
+    printf("Enter range: ");
+    scanf("%d %d", &start, &end);
+
+    printf("Prime numbers are:\n");
+
+    for (i = start; i <= end; i++) {
+        if (i <= 1)
+            continue;
+
+        prime = 1;
+
+        for (j = 2; j <= i / 2; j++) {
+            if (i % j == 0) {
+                prime = 0;
+                break;
+            }
+        }
+
+        if (prime)
+            printf("%d ", i);
+    }
+
+    return 0;
+}
