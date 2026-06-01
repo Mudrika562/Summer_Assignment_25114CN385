4017d3f37ae394dc029276d3da48c57c499f5c96
Mudrika Mishra <mudrika.mishra07@gmail.com>
2026-06-01 22:42:15 +0530

    code3.cpp

--- DAY 3/code3.cpp
+++ DAY 3/code3.cpp
@@ -1,19 +1,19 @@
 //GCD of two numbers
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 int main() {
-    int a, b, i, gcd;
+    int a, b, gcd;
 
-    printf("Enter two numbers: ");
-    scanf("%d %d", &a, &b);
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
 
-    for (i = 1; i <= a && i <= b; i++) {
-        if (a % i == 0 && b % i == 0) {
+    for (int i = 1; i <= a && i <= b; i++) {
+        if (a % i == 0 && b % i == 0)
             gcd = i;
-        }
     }
 
-    printf("GCD = %d", gcd);
+    cout << "GCD = " << gcd;
 
     return 0;
 }
