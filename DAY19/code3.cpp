1d535be33b18a066af8c9c7ea6fb04fdfe793993
Mudrika Mishra <mudrika.mishra07@gmail.com>
2026-06-19 18:40:56 +0530

    DAY19 coding questions

---
+++ DAY19/code3.cpp
@@ -0,0 +1,25 @@
+//Transpose Matrix
+#include <iostream>
+using namespace std;
+
+int main() {
+    int r, c;
+    cout << "Enter rows and columns: ";
+    cin >> r >> c;
+
+    int a[r][c];
+
+    cout << "Enter matrix elements:\n";
+    for(int i = 0; i < r; i++)
+        for(int j = 0; j < c; j++)
+            cin >> a[i][j];
+
+    cout << "Transpose Matrix:\n";
+    for(int j = 0; j < c; j++) {
+        for(int i = 0; i < r; i++)
+            cout << a[i][j] << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
